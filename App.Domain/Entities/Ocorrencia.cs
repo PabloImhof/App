2b6a46c10b06196c8bfa6dee133d9b31da8be6c8
PabloImhof <89610302+PabloImhof@users.noreply.github.com>
2021-09-08 23:14:04 -0300

    Adicionar arquivos de projeto.

---
+++ App.Domain/Entities/Ocorrencia.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Domain.Entities
+{
+    public class Ocorrencia
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public Guid PessoaId { get; set; }
+        public DateTime Dataocorrencia { get; set; }
+        public DateTime? DataBaixaOcorrencia { get; set; }
+        public string Observacao { get; set; }
+        public Pessoa Pessoa { get; set; }
+
+    }
+}
---
+++ App.Persistence/AppDBContext.cs
@@ -0,0 +1,27 @@
+﻿using App.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Persistence
+{
+
+    public class AppDBContext : DbContext 
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        { 
+        }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDBContext).Assembly);
+        }
+        public DbSet<Pessoa> Pessoa { get; set; }
+        public DbSet<Ocorrencia> Ocorrencia { get; set; }
+
+
+    }
+
+}
